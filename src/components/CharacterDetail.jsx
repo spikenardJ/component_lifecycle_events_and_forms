1ba62fa1c85b2fa3ecdf0a4d8dab5b3bded43936
spikenardJ <jennifer_annette@hotmail.com>
2025-01-08 15:00:16 -0600

    Third commit

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -7,7 +7,7 @@ const CharacterDetail = ({ characterId }) => {
   const publicKey = "4d52fb3fa1ef52af3d6b38218aff5477";
   const ts = 1;
   const hash = "a80ea7d0806646c1f3b6bf37422fc6fd";
-  const apiUrl = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+  const apiUrl = `http://gateway.marvel.com/v1/public/characters/${characterId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
   useEffect(() => {
     const fetchCharacterDetail = async () => {
